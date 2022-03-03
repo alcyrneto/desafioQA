a86b6e7c69ff8e6c534120ce74fc805123479538
Alcyr Oliveira Porciuncula Neto <alcyr.neto@levelupgames.com.br>
2022-03-03 18:48:17 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace desafioQA.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Robson Soares e Clever dos santos";
+        }
+        [HttpPost]
+        public string Post(string name)
+        {
+            if (name == "Robson")
+            {
+                return ("Soares");
+            }
+            else if (name == "Clever")
+            {
+                return ("dos santos");
+            }
+            else
+            {
+                return ("deu ruim");
+            }
+        }
+    }
+}
